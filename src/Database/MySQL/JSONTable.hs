7c314567144fdf3ee8dc69f6511f62291b7574e1
Daniel Casanueva <daniel.casanueva@proton.me>
2023-05-13 17:03:05 +0200

    insert and lookup

--- src/Database/MySQL/JSONTable.hs
+++ src/Database/MySQL/JSONTable.hs
@@ -1,35 +1,50 @@
 ----------------------------------------------------------------------------------------------------
 module Database.MySQL.JSONTable
-  ( ID
+  ( -- * Types
+    Id
   , JSONTable (..)
+    -- * Table operations
   , createTable
   , deleteTable
+    -- * Row operations
+  , insert
+  , lookup
     ) where
 
+import Prelude hiding (lookup)
 import Data.Word
 import Data.String (fromString)
-import Data.Text (Text)
-import Data.Text qualified as Text
+import Data.Maybe (listToMaybe)
+import Data.Typeable (Typeable)
+import Data.ByteString qualified as ByteString
 import Database.MySQL.Simple qualified as SQL
+import Database.MySQL.Base.Types qualified as SQLTypes
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Aeson qualified as JSON
 
 -- | Row identifier used for table lookups.
 --   The type parameter indicates the type of data
 --   stored in the table.
-newtype ID a = ID { fromID :: Word64 }
+newtype Id a = Id { fromId :: Word64 } deriving (Eq, Ord, Show)
+
+instance SQL.ToField (Id a) where
+  toField = fromString . show . fromId
+
+instance SQL.Param (Id a)
 
 -- | A MySQL table with two columns:
 --
 -- +----------------------------+-----------+
 -- |             id             |    data   |
 -- +============================+===========+
--- | Row identifier (type 'ID') | JSON data |
+-- | Row identifier (type 'Id') | JSON data |
 -- +----------------------------+-----------+
 --
 -- The type parameter indicates the type of data
 --  stored in the table.
 data JSONTable a = JSONTable
   { -- | Table name.
-    tableName :: Text
+    tableName :: String
     }
 
 tableSpecs :: String
@@ -45,17 +60,17 @@ tableSpecs = concat
 createTable
   :: SQL.Connection -- ^ MySQL database connection.
   -> Bool -- ^ Fail if table already exists.
-  -> Text -- ^ Table name.
+  -> String -- ^ Table name.
   -> IO (JSONTable a)
 createTable conn failIfExists name = do
   let ifNotExists = if failIfExists then " " else " IF NOT EXISTS "
-      query = "CREATE TABLE" ++ ifNotExists ++ "`" ++ Text.unpack name ++ "` " ++ tableSpecs
+      query = "CREATE TABLE" ++ ifNotExists ++ "`" ++ name ++ "` " ++ tableSpecs
   _ <- SQL.execute conn (fromString query) ()
   pure $ JSONTable
     { tableName = name
       }
 
--- | Delete a JSON table from a MySQL database.
+-- | Delete a JSON table from a MySQL database, together with all of its content.
 deleteTable
   :: SQL.Connection -- ^ MySQL database connection.
   -> Bool -- ^ Fail if table doesn't exist.
@@ -63,6 +78,41 @@ deleteTable
   -> IO ()
 deleteTable conn failIfNotExist table = do
   let ifExists = if failIfNotExist then " " else " IF EXISTS "
-      query = "DROP TABLE" ++ ifExists ++ "`" ++ Text.unpack (tableName table) ++ "`"
+      query = "DROP TABLE" ++ ifExists ++ "`" ++ tableName table ++ "`"
   _ <- SQL.execute conn (fromString query) ()
   pure ()
+
+-- | JSON serialization helper.
+newtype AsJSON a = AsJSON { asJSON :: a }
+
+instance FromJSON a => SQL.FromField (AsJSON a) where
+  fromField = ([SQLTypes.Json], fmap AsJSON . JSON.eitherDecodeStrict)
+
+instance ToJSON a => SQL.ToField (AsJSON a) where
+  toField = ByteString.toStrict . JSON.encode . asJSON
+
+instance (Typeable a, FromJSON a) => SQL.Result (AsJSON a)
+instance ToJSON a => SQL.Param (AsJSON a)
+
+-- | Insert a new row into a table.
+insert
+  :: ToJSON a
+  => SQL.Connection -- ^ MySQL database connection.
+  -> JSONTable a -- ^ Table to insert the new row.
+  -> a -- ^ Data for the new row.
+  -> IO (Id a) -- ^ Identifier of the new row.
+insert conn table x = do
+  let query = "INSERT INTO `" ++ tableName table ++ "` (data) VALUES (?)"
+  _ <- SQL.execute conn (fromString query) $ SQL.Only $ AsJSON x
+  Id <$> SQL.insertID conn
+
+-- | Lookup a row in a table.
+lookup
+  :: (Typeable a, FromJSON a)
+  => SQL.Connection -- ^ MySQL database connection.
+  -> JSONTable a -- ^ Table to insert the new row.
+  -> Id a -- ^ Identifier to use for the table lookup.
+  -> IO (Maybe a)
+lookup conn table i = do
+  let query = "SELECT data FROM `" ++ tableName table ++ "` WHERE id=?"
+  fmap (asJSON . SQL.fromOnly) . listToMaybe <$> SQL.query conn (fromString query) (SQL.Only i)
--- test/Main.hs
+++ test/Main.hs
@@ -1,8 +1,11 @@
 
 module Main (main) where
 
+import Prelude hiding (lookup)
 import Database.MySQL.Simple qualified as SQL
 import Database.MySQL.JSONTable
+import Data.Ratio (Ratio, (%))
+import Control.Monad (unless)
 
 main :: IO ()
 main = do
@@ -11,7 +14,12 @@ main = do
     , SQL.connectUser = "root"
     , SQL.connectPassword = "root"
       })
-  table <- createTable conn True "create-delete-table"
-  _ <- createTable conn False $ tableName table
-  deleteTable conn True table
-  deleteTable conn False table
+  table0 <- createTable conn True "create-delete-table"
+  _ <- createTable conn False $ tableName table0
+  deleteTable conn True table0
+  deleteTable conn False table0
+  table <- createTable conn True "int-table"
+  let half :: Ratio Int
+      half = 1 % 2
+  x <- insert conn table half >>= lookup conn table
+  unless (x == Just half) $ fail $ "Insert-Lookup test failed: got " ++ show x
